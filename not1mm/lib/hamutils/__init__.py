1390ac618b8310cb1223ba441babd12a0b2fa6bf
Kyle Boyle <kyle@kyleboyle.com>
2024-04-15 18:38:11 -0300

    poached hamutil - suspect it'll need customization

---
+++ not1mm/lib/hamutils/__init__.py
@@ -0,0 +1,8 @@
+"""poached from https://github.com/sq8kfh/hamutils/commit/9afe06850aea3adaa610292fd46f4d29dcc41c72"""
+
+"""Amateur Radio utility library by SQ8KFH"""
+
+
+__all__ = ['adif', 'cabrillo', 'qrz', 'space_weather']
+
+__version__ = "0.2.0"
